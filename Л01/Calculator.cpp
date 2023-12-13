1b8b52c9aa311957538bb7e53a5a8e9e8573a8c5
ramanyyeww <149940801+ramanyyeww@users.noreply.github.com>
2023-12-13 14:45:13 +0300

    Add files via upload

---
+++ Л01/Calculator.cpp
@@ -0,0 +1,50 @@
+﻿
+#include <iostream>
+#include <clocale>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    char op;
+    double num1, num2;
+
+    bool exit = false;
+    while (!exit) {
+        cout << "Введите оператор (+, -, *, /) или 'q' для выхода: ";
+        cin >> op;
+
+        if (op == 'q') {
+            exit = true;
+            continue;
+        }
+
+        cout << "Введите два числа: ";
+        cin >> num1 >> num2;
+
+        switch (op) {
+        case '+':
+            cout << num1 << " + " << num2 << " = " << num1 + num2;
+            break;
+        case '-':
+            cout << num1 << " - " << num2 << " = " << num1 - num2;
+            break;
+        case '*':
+            cout << num1 << " * " << num2 << " = " << num1 * num2;
+            break;
+        case '/':
+            if (num2 != 0)
+                cout << num1 << " / " << num2 << " = " << num1 / num2;
+            else
+                cout << "Ошибка: деление на ноль";
+            break;
+        default:
+            cout << "Ошибка: неправильный оператор";
+            break;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
